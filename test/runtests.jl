2ce6f51abf644130da65522c272642003b46cfab
Fengyang Wang <fengyang.wang.0@gmail.com>
2015-11-30 10:29:00 -0500

    Add system for getting base units for other currencies.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,9 +2,9 @@ using Currencies
 using Base.Test
 
 # arithmetic
-@test 1usd + 2usd == 3usd
-@test 1.5usd * 3 == 4.5usd
-@test 1.11usd * 999 == 1108.89usd
+@test 1USD + 2USD == 3USD
+@test 1.5USD * 3 == 4.5USD
+@test 1.11USD * 999 == 1108.89USD
 
 # no mixing types
-@test_throws MethodError 1usd + 1cad
+@test_throws MethodError 1USD + 1CAD
