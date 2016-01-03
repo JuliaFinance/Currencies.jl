bf237d1d8608757a0e7601a26c2dd40b0fce05ba
Fengyang Wang <fengyang.wang.0@gmail.com>
2016-01-02 20:49:45 -0500

    Add a test for LOCAL_SYMBOL_LOCATION

--- test/data.jl
+++ test/data.jl
@@ -7,10 +7,16 @@
 
     # symbols should all be registered currencies
     registeredcurrencies = keys(Currencies.DATA)
-    for symbol in keys(Currencies.SHORT_SYMBOL)
-        @test symbol ∈ registeredcurrencies
+    for sym in keys(Currencies.SHORT_SYMBOL)
+        @test sym ∈ registeredcurrencies
     end
-    for symbol in keys(Currencies.LONG_SYMBOL)
-        @test symbol ∈ registeredcurrencies
+    for sym in keys(Currencies.LONG_SYMBOL)
+        @test sym ∈ registeredcurrencies
+    end
+
+    # locale keys should be registered currencies, and value :before or :after
+    for (sym, val) in Currencies.LOCAL_SYMBOL_LOCATION
+        @test sym ∈ registeredcurrencies
+        @test val ∈ (:before, :after)
     end
 end
